887a7e7156f23e3aea5ccb0e07361576713c4bf0
cieloazul310 <cieloazul310@gmail.com>
2022-11-20 20:29:48 +0900

    chore: initial commit

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
-# Storybook, React, TypeScript and MUI Template
+# react-swipeable-views and Swiper with MUI
 
-- [Storybook]
-- [React]
-- [TypeScript]
-- [MUI]
-- [ESLint]
-- [Prettier]
+[react-swipeable-views] is now deprecated.
+
+Deprecate package, do not use #676  
+<https://github.com/oliviertassinari/react-swipeable-views/issues/676>
+
+Swiper  
+<https://swiperjs.com/>
 
 ## Demo
 
-<https://cieloazul310.github.io/react-storybook-mui-typescript>
+Built with [Storybook]
+<https://cieloazul310.github.io/mui-swiper>
 
 ## Reference
 
 Material UI in Storybook  
 <https://storybook.js.org/blog/material-ui-in-storybook/>
 
+[react-swipeable-views]: https://react-swipeable-views.com/ "react-swipeable-views"
 [Storybook]: https://storybook.js.org/ "Storybook"
-[React]: https://reactjs.org/ "React"
-[TypeScript]: https://www.typescriptlang.org/ "TypeScript"
-[MUI]: https://mui.com/ "MUI"
-[ESLint]: https://eslint.org/ "ESLint"
-[Prettier]: https://prettier.io/ "Prettier"
---
+++ src/stories/react-swipeable-views.stories.tsx
@@ -0,0 +1,137 @@
+/* eslint react/jsx-props-no-spreading: off */
+import * as React from 'react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import SwipeableViews from 'react-swipeable-views';
+import { bindKeyboard } from 'react-swipeable-views-utils';
+
+const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'React Swipeable Views',
+  component: SwipeableViews,
+} as ComponentMeta<typeof SwipeableViews>;
+
+type TabPanelProps = React.PropsWithChildren<{
+  index: number;
+  value: number;
+}>;
+
+function TabPanel({ children, value, index }: TabPanelProps) {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+export const Basic: ComponentStory<typeof SwipeableViews> = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <Box sx={{ bgcolor: 'background.paper', width: 1 }}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews index={value} onChangeIndex={handleChangeIndex}>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
+    </Box>
+  );
+};
+
+export const BindKeyboard: ComponentStory<typeof SwipeableViews> = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <Box sx={{ bgcolor: 'background.paper', width: 1 }}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <BindKeyboardSwipeableViews
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </BindKeyboardSwipeableViews>
+    </Box>
+  );
+};
